f52237027cd7c15976e396b1ced24f7e564fef4c
merzann <annisch78@gmail.com>
2024-05-14 18:15:16 +0000

    style: add background image and style footer sections

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,14 +1,12 @@
-<script>
+const newLocal = <script>
     const video = document.getElementById('video-player');
     const volumeBtn = document.getElementById('volume-btn');
 
-    volumeBtn.addEventListener('click', () => {
-        if (video.muted) {
-            video.muted = false;
-            volumeBtn.textContent = 'Mute';
-        } else {
-            video.muted = true;
-            volumeBtn.textContent = 'Unmute';
-        }
-    });
-</script>
+    volumeBtn.addEventListener('click', () ={">"} { }
+    if (video.muted) {video.muted = false};
+    volumeBtn.textContent = 'Mute';
+    &rbrace; else {video.muted = true};
+    volumeBtn.textContent = 'Unmute';
+    {"}"}
+    {"}"});
+</script>;
